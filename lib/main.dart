7d54c1d760181f75042dfeb1ddec6b18749b0234
Travis-ugo <68594765+Travis-ugo@users.noreply.github.com>
2021-06-09 07:25:46 +0100

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,189 +1,157 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'button.dart';
+import 'color.dart';
+import 'riverpod.dart';
 
-void main() {
-  runApp(Home());
-}
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
 
-class Home extends StatelessWidget {
-  const Home({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyApp(),
-    );
-  }
+  runApp(ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  static final String title = 'Calculator';
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        title: Container(
-          margin: EdgeInsets.only(left: 8),
-          child: Text('Calculator'),
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        theme: ThemeData(
+          scaffoldBackgroundColor: MyColors.background1,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-      ),
-      body: SafeArea(
+        home: MainPage(title: title),
+      );
+}
+
+class MainPage extends StatefulWidget {
+  final String title;
+
+  const MainPage({
+    @required this.title,
+  });
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          title: Container(
+            margin: EdgeInsets.only(left: 8),
+            child: Text(widget.title),
+          ),
+        ),
+        body: SafeArea(
           child: Column(
-        children: [
-          Expanded(child: buildResult()),
-          Expanded(
-            flex: 2,
-            child: buildButtons(),
+            children: <Widget>[
+              Expanded(child: buildResult()),
+              Expanded(flex: 2, child: buildButtons())
+            ],
           ),
-        ],
-      )),
-    );
-  }
-}
-
-Widget buildResult() => Container(
-      padding: const EdgeInsets.all(24),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: [
-          Text(
-            '0',
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 36,
-            ),
-          ),
-          SizedBox(height: 24),
-          Text(
-            '0',
-            overflow: TextOverflow.ellipsis,
-            style: TextStyle(
-              color: Colors.grey,
-              fontSize: 18,
-            ),
-          ),
-        ],
-      ),
-    );
-
-Widget buildButtons() => Container(
-      padding: EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.vertical(
-          top: Radius.circular(32),
         ),
-      ),
-      child: Column(
-        children: <Widget>[
-          buildButtonRow('AC', '<x', '', '/'),
-          buildButtonRow('7', '8', '9', 'x'),
-          buildButtonRow('4', '5', '6', '-'),
-          buildButtonRow('1', '2', '3', '+'),
-          buildButtonRow('0', '.', '', '='),
-        ],
-      ),
-    );
+      );
 
-Widget buildButtonRow(
-  String first,
-  String second,
-  String third,
-  String fourth,
-) {
-  final row = [first, second, third, fourth];
-  return Expanded(
-    child: Row(
-      children: row
-          .map(
-            (text) => ButtonWidget(
-              text: text,
-              onClicked: () => print(text),
-              onClickedLong: () => print(text),
+  Widget buildResult() => Consumer(
+        builder: (context, watch, child) {
+          // final state = watch(calculatorProvider);
+          final state = watch(calculatorProvider);
+          return Container(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Text(
+                  state.equation,
+                  overflow: TextOverflow.ellipsis,
+                  style:
+                      TextStyle(color: Colors.white, fontSize: 36, height: 1),
+                ),
+                const SizedBox(height: 24),
+                Text(
+                  state.result,
+                  overflow: TextOverflow.ellipsis,
+                  style: TextStyle(color: Colors.grey, fontSize: 18),
+                ),
+              ],
             ),
-          )
-          .toList(),
-    ),
-  );
-}
+          );
+        },
+      );
 
-class ButtonWidget extends StatelessWidget {
-  final String text;
-  final VoidCallback onClicked;
-  final VoidCallback onClickedLong;
+  Widget buildButtons() => Container(
+        padding: EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          color: MyColors.background2,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(32)),
+        ),
+        child: Column(
+          children: <Widget>[
+            buildButtonRow('AC', '<', '', '÷'),
+            buildButtonRow('7', '8', '9', '⨯'),
+            buildButtonRow('4', '5', '6', '-'),
+            buildButtonRow('1', '2', '3', '+'),
+            buildButtonRow('0', '.', '', '='),
+          ],
+        ),
+      );
 
-  const ButtonWidget({
-    Key key,
-    @required this.text,
-    @required this.onClicked,
-    @required this.onClickedLong,
-  }) : super(key: key);
+  Widget buildButtonRow(
+    String first,
+    String second,
+    String third,
+    String fourth,
+  ) {
+    final row = [first, second, third, fourth];
 
-  @override
-  Widget build(BuildContext context) {
-    final color = getTextColor(text);
-    final double fontSize = Utils.isOperator(text, hasEquals: true) ? 26 : 22;
-    final style = TextStyle(
-      color: color,
-      fontSize: fontSize,
-      fontWeight: FontWeight.bold,
-    );
     return Expanded(
-      child: Container(
-        height: double.infinity,
-        margin: EdgeInsets.all(6),
-        child: ElevatedButton(
-          onPressed: onClicked,
-          onLongPress: onClickedLong,
-          child: text == '<x'
-              ? Icon(
-                  Icons.backspace_outlined,
-                  color: color,
-                )
-              : Text(text, style: style),
-          style: ElevatedButton.styleFrom(
-              primary: MyColors.background3,
-              elevation: 0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
-              )),
-        ),
+      child: Row(
+        children: row
+            .map((text) => ButtonWidget(
+                  text: text,
+                  onClicked: () => onClickedButton(text),
+                  onClickedLong: () => onLongClickedButton(text),
+                ))
+            .toList(),
       ),
     );
   }
-}
 
-Color getTextColor(String buttonText) {
-  switch (buttonText) {
-    case '+':
-    case '-':
-    case 'x':
-    case '/':
-    case '=':
-      return MyColors.operators;
-    case 'AC':
-    case '<x':
-      return MyColors.delete;
-    default:
-      return MyColors.numbers;
+  void onClickedButton(String buttonText) {
+    final calculator = context.read(calculatorProvider);
+
+    switch (buttonText) {
+      case 'AC':
+        calculator.reset();
+        break;
+      case '=':
+        calculator.equals();
+        break;
+      case '<':
+        calculator.delete();
+        break;
+      default:
+        calculator.append(buttonText);
+        break;
+    }
   }
-}
 
-class Utils {
-  static bool isOperator(String buttonText, {bool hasEquals = false}) {
-    final operators = ['+', '-', '+', 'x', '.']..addAll(hasEquals ? ['='] : []);
-    return operators.contains(buttonText);
+  void onLongClickedButton(String text) {
+    final calculator = context.read(calculatorProvider);
+
+    if (text == '<') {
+      calculator.reset();
+    }
   }
-}
-
-class MyColors {
-  static final Color background1 = Color(0xff22252d);
-  static final Color background2 = Color(0xff292d36);
-  static final Color background3 = Color(0xff272b33);
-  static final Color operators = Color(0xfff57b7b);
-  static final Color delete = Color(0xff26f4ce);
-  static final Color numbers = Color(0xffffffff);
 }
---
+++ lib/riverpod.dart
@@ -0,0 +1,78 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:math_expressions/math_expressions.dart';
+import 'calculator.dart';
+import 'utils.dart';
+
+final calculatorProvider = StateNotifierProvider((ref) => CalculatorNotifier());
+
+class CalculatorNotifier extends StateNotifier<Calculator> {
+  CalculatorNotifier() : super(Calculator());
+
+  void delete() {
+    final equation = state.equation;
+
+    if (equation.isNotEmpty) {
+      final newEquation = equation.substring(0, equation.length - 1);
+
+      if (newEquation.isEmpty) {
+        reset();
+      } else {
+        state = state.copy(equation: newEquation);
+        calculate();
+      }
+    }
+  }
+
+  void reset() {
+    final equation = '0';
+    final result = '0';
+    state = state.copy(equation: equation, result: result);
+  }
+
+  void resetResult() {
+    final equation = state.result;
+
+    state = state.copy(
+      equation: equation,
+      shouldAppend: false,
+    );
+  }
+
+  void append(String buttonText) {
+    final equation = () {
+      if (Utils.isOperator(buttonText) &&
+          Utils.isOperatorAtEnd(state.equation)) {
+        final newEquation =
+            state.equation.substring(0, state.equation.length - 1);
+
+        return newEquation + buttonText;
+      } else if (state.shouldAppend) {
+        return state.equation == '0' ? buttonText : state.equation + buttonText;
+      } else {
+        return Utils.isOperator(buttonText)
+            ? state.equation + buttonText
+            : buttonText;
+      }
+    }();
+
+    state = state.copy(equation: equation, shouldAppend: true);
+    calculate();
+  }
+
+  void equals() {
+    calculate();
+    resetResult();
+  }
+
+  void calculate() {
+    final expression = state.equation.replaceAll('⨯', '*').replaceAll('÷', '/');
+
+    try {
+      final exp = Parser().parse(expression);
+      final model = ContextModel();
+
+      final result = '${exp.evaluate(EvaluationType.REAL, model)}';
+      state = state.copy(result: result);
+    } catch (e) {}
+  }
+}
